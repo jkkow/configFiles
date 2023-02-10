34c4e3832ed77a2118b0da272757071acc5bbf89
jkkow <jkwonko@gmail.com>
2023-02-10 09:35:16 +0900

    Change alias in powershell profile

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -3,8 +3,8 @@ New-Alias -Name vim -Value nvim  # New-Alias for setting new alias
 Set-Alias ll ls # Set-Alias for change existing alias to another
 function lld { Get-ChildItem -Directory}
 function gs { & git status }
-function gcout { & git checkout $args }
-function ac { & git add --all ; & git commit -m $args}
+function gch { & git checkout $args }
+function gac { & git add --all ; & git commit -m $args}
 
 Import-Module Terminal-Icons
 
