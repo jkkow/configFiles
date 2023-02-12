0127393d368318ad887850fc2c21bba7b2d6d373
jkkow <jkwonko@gmail.com>
2023-02-13 00:29:56 +0900

    Add alias 'cwd'

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -1,10 +1,11 @@
 # Alias
-New-Alias -Name vim -Value nvim  # New-Alias for setting new alias
+New-Alias -Name v -Value nvim  # New-Alias for setting new alias
 Set-Alias ll ls # Set-Alias for change existing alias to another
 function lld { Get-ChildItem -Directory}
 function gs { & git status }
 function gch { & git checkout $args }
-function gac { & git add --all ; & git commit -m $args}
+function gac { & git add --all ; & git commit -m $args }
+function cwd { (pwd).path | Set-Clipboard }
 
 Import-Module Terminal-Icons
 
